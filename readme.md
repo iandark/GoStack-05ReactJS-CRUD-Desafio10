168c5f8c31d60a2351cdad6aa2f9edb0f8ca6f6c
Derkian Souza <derkian@iands.com.br>
2020-11-30 17:28:31 -0300

    feat: Inclusão de projeto funcional

---
+++ readme.md
@@ -0,0 +1,4 @@
+# Desafio GoRestauranteWeb
+Aplicação em React.js junto com TypeScript, praticando o conceito de CRUD (Create, Read, Update, Delete)
+
+Link para o desafio original: [https://github.com/rocketseat-education/bootcamp-gostack-desafios/tree/master/desafio-reactjs-crud](https://github.com/rocketseat-education/bootcamp-gostack-desafios/tree/master/desafio-reactjs-crud)
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -27,7 +27,9 @@ const Dashboard: React.FC = () => {
 
   useEffect(() => {
     async function loadFoods(): Promise<void> {
-      // TODO LOAD FOODS
+      const response = await api.get('/foods');
+
+      setFoods(response.data);
     }
 
     loadFoods();
@@ -37,7 +39,14 @@ const Dashboard: React.FC = () => {
     food: Omit<IFoodPlate, 'id' | 'available'>,
   ): Promise<void> {
     try {
-      // TODO ADD A NEW FOOD PLATE TO THE API
+      const addedFood = {
+        ...food,
+        id: foods.length + 1,
+        available: true,
+      };
+
+      await api.post('/foods', addedFood);
+      setFoods([...foods, addedFood]);
     } catch (err) {
       console.log(err);
     }
@@ -46,11 +55,25 @@ const Dashboard: React.FC = () => {
   async function handleUpdateFood(
     food: Omit<IFoodPlate, 'id' | 'available'>,
   ): Promise<void> {
-    // TODO UPDATE A FOOD PLATE ON THE API
+    const updatedFood = {
+      ...food,
+      id: editingFood.id,
+      available: editingFood.available,
+    };
+
+    const updatedFoods = foods.map(normalFood =>
+      normalFood === editingFood ? updatedFood : normalFood,
+    );
+
+    await api.put(`/foods/${editingFood.id}`, updatedFood);
+    setFoods(updatedFoods);
   }
 
   async function handleDeleteFood(id: number): Promise<void> {
-    // TODO DELETE A FOOD PLATE FROM THE API
+    const filteredFoods = foods.filter(food => food.id !== id);
+
+    await api.delete(`/foods/${id}`);
+    setFoods(filteredFoods);
   }
 
   function toggleModal(): void {
@@ -62,7 +85,8 @@ const Dashboard: React.FC = () => {
   }
 
   function handleEditFood(food: IFoodPlate): void {
-    // TODO SET THE CURRENT EDITING FOOD ID IN THE STATE
+    setEditingFood(food);
+    toggleEditModal();
   }
 
   return (
